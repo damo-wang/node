269b526c2a29c9c2480fa9c86a7f4189abee1fc9
damo-wang <hemoparrot@qq.com>
2017-08-17 22:54:56 +0800

    20170817 by wangyuanjie

--- basic/buffer/read.js
+++ basic/buffer/read.js
@@ -1,23 +1,4 @@
-var fs = require("fs");
-var data = '';
+buf = new Buffer('www.helloqingcong.com');
 
-// 创建可读流
-var readerStream = fs.createReadStream('input.txt');
-
-// 设置编码为 utf8。
-readerStream.setEncoding('UTF8');
-
-// 处理流事件 --> data, end, and error
-readerStream.on('data', function(chunk) {
-    data += chunk;
-});
-
-readerStream.on('end', function() {
-    console.log(data);
-});
-
-readerStream.on('error', function(err) {
-    console.log(err.stack);
-});
-
-console.log("程序执行完毕");
+console.log(buf.toString());
+console.log(buf.length);
