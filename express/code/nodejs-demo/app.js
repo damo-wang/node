8342fe2e95be03f31dd4ca7d39a95401175dfe47
damo-wang <hemoparrot@qq.com>
2017-08-18 17:30:50 +0800

    wangyuanjie 20170818

--- express/code/nodejs-demo/app.js
+++ express/code/nodejs-demo/app.js
@@ -25,22 +25,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
+//增加对/  /home  /login  /logout的路由 by wangyuanjie
+app.use('/home', home);
+app.use('/login', login);
+app.use('/logout', logout);
+//增加路由结束 wangyuanjie
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- express/code/nodejs-demo/routes/home.js
+++ express/code/nodejs-demo/routes/home.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'QingCong Index' });
+    var user = {
+        username: 'admin',
+        password: '123456'
+    };
+    res.render('home', { title: 'Home', user: user });
 });
 
 module.exports = router;
