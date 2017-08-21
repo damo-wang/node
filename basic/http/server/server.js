9349b64c01a49c565c642e61731a23eeefcc691e
damo-wang <hemoparrot@qq.com>
2017-08-21 11:05:29 +0800

    20170821 by wangyuanjie

--- basic/http/server/server.js
+++ basic/http/server/server.js
@@ -4,9 +4,10 @@ var url = require('url');
 
 
 // 创建服务器
-http.createServer(function(request, response) {
+//http.createServer(function(request, response) {
+var fun = function(request, response) {
     // 解析请求，包括文件名
-    console.log('request:');
+    //console.log('request:');
     console.log(request);
     var pathname = url.parse(request.url).pathname;
 
@@ -31,7 +32,9 @@ http.createServer(function(request, response) {
         //  发送响应数据
         response.end();
     });
-}).listen(8081);
+}
+var app = http.createServer(fun);
+app.listen(8081);
 
 // 控制台会输出以下信息
 console.log('Server running at http://127.0.0.1:8081/');
---
+++ express/code/myREST/index.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var app = express();
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var jade = require('jade');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.get('/listUsers', function(req, res) {
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
+        var id = url.parse(req.url, true).query.id;
+        console.log('id:' + id);
+        if (id != undefined) {
+            data = JSON.parse(data);
+            var user = data["user" + id];
+            console.log(user);
+            res.render('list', {
+                title: '用户信息',
+                juser: user
+            });
+        } else {
+            console.log(data);
+            data = JSON.parse(data);
+            res.render('list', {
+                title: '用户信息',
+                juser: data
+            });
+        }
+    });
+});
+
+//添加的新用户数据
+var user = {
+    "user4": {
+        "name": "mohit",
+        "password": "password4",
+        "profession": "teacher",
+        "id": 4
+    }
+};
+
+app.get('/addUser', function(req, res) {
+    // 读取已存在的数据
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
+        data = JSON.parse(data);
+        data["user4"] = user["user4"];
+
+        fs.writeFile(__dirname + "/" + "users.json", JSON.stringify(data), function(err) {
+            if (err) {
+                res.end('add failed');
+            } else {
+                res.end('add success');
+            }
+        });
+        console.log(data);
+        res.end(JSON.stringify(data));
+
+    });
+});
+
+app.get('/deleteUser', function(req, res) {
+
+    // First read existing users.
+    var id = url.parse(req.url, true).query.id;
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
+        data = JSON.parse(data);
+        delete data["user" + id];
+        fs.writeFile(__dirname + "/" + "users.json", JSON.stringify(data), function(err) {
+            if (err) {
+                res.end('add failed');
+            } else {
+                res.end('add success');
+            }
+        });
+        res.end(JSON.stringify(data));
+    });
+});
+/*
+app.get('/:id', function(req, res) {
+    // 首先我们读取已存在的用户
+    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data) {
+        data = JSON.parse(data);
+        var user = data["user" + req.params.id]
+        console.log(user);
+        res.end(JSON.stringify(user));
+    });
+});
+*/
+var server = app.listen(3000, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('app listening at http://%s:%s', host, port);
+});
